42df9fac476f8deb47b297aae66db5bc874a331b
GurtejSingh <GurtejSingh@DESKTOP-93LSR4J>
2023-10-08 21:25:51 +0530

    Implementing Paging and Sorting

--- blog-api-apis/src/main/java/com/codewithgurtej/blog/services/impl/PostServiceImpl.java
+++ blog-api-apis/src/main/java/com/codewithgurtej/blog/services/impl/PostServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.codewithgurtej.blog.entities.Category;
@@ -77,9 +78,10 @@ public class PostServiceImpl implements PostService {
 	}
 
 	@Override
-	public PostResponse getAllPost(Integer pageNumber, Integer pageSize) {
+	public PostResponse getAllPost(Integer pageNumber, Integer pageSize, String sortBy, String sortDir) {
 		
-		Pageable p = PageRequest.of(pageNumber, pageSize);
+		Sort sort = (sortDir.equalsIgnoreCase("asc"))?sort = Sort.by(sortBy).ascending():Sort.by(sortBy).descending();
+		Pageable p = PageRequest.of(pageNumber, pageSize, sort);
 		
 		Page<Post> pagePost = this.postRepo.findAll(p);
 		List<Post> allPost = pagePost.getContent();
