f8ac794c678f58cfe41bd2eb6bda0ebdb9923599
GurtejSingh <GurtejSingh@DESKTOP-93LSR4J>
2023-10-08 21:50:29 +0530

    Implementing Searching

--- blog-api-apis/src/main/java/com/codewithgurtej/blog/controllers/PostController.java
+++ blog-api-apis/src/main/java/com/codewithgurtej/blog/controllers/PostController.java
@@ -85,4 +85,12 @@ public class PostController {
 		PostDto updatedPost = this.postService.updatePost(postDto, postId);
 		return new ResponseEntity<PostDto>(updatedPost, HttpStatus.OK);
 	}
+	
+	//Search Post By Title
+	@GetMapping("/posts/search/{keywords}")
+	public ResponseEntity<List<PostDto>> searchPostByTitle(@PathVariable("keywords") String keywords){
+		List<PostDto> result = this.postService.searchPosts(keywords);
+		return new ResponseEntity<List<PostDto>>(result, HttpStatus.OK);
+	}
+	
 }
