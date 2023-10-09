c468896de628600b1458688d5b9a5081282e55ad
GurtejSingh <GurtejSingh@DESKTOP-93LSR4J>
2023-10-09 22:21:47 +0530

    Introduce Application Constants

--- blog-api-apis/src/main/java/com/codewithgurtej/blog/controllers/PostController.java
+++ blog-api-apis/src/main/java/com/codewithgurtej/blog/controllers/PostController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codewithgurtej.blog.config.AppConstants;
 import com.codewithgurtej.blog.payloads.ApiResponse;
 import com.codewithgurtej.blog.payloads.PostDto;
 import com.codewithgurtej.blog.payloads.PostResponse;
@@ -56,10 +57,10 @@ public class PostController {
 	// Get All Post
 	@GetMapping("/posts")
 	public ResponseEntity<PostResponse> getAllPosts(
-			@RequestParam(value = "pageNumber",defaultValue = "0",required = false) Integer pageNumber,
-			@RequestParam(value = "pageSize",defaultValue = "10",required = false) Integer pageSize,
-			@RequestParam(value = "sortBy", defaultValue = "postId", required = false) String sortBy,
-			@RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir			
+			@RequestParam(value = "pageNumber",defaultValue = AppConstants.PAGE_NUMBER,required = false) Integer pageNumber,
+			@RequestParam(value = "pageSize",defaultValue = AppConstants.PAGE_SIZE,required = false) Integer pageSize,
+			@RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
+			@RequestParam(value = "sortDir", defaultValue = AppConstants.SORT_DIR, required = false) String sortDir			
 			){
 		PostResponse postResponse = this.postService.getAllPost(pageNumber, pageSize, sortBy, sortDir);
 		return new ResponseEntity<PostResponse>(postResponse, HttpStatus.OK);
