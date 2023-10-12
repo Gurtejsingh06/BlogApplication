6e10c392b3ea62d0a1d51b4af6c212c7cb5cb634
GurtejSingh <GurtejSingh@DESKTOP-93LSR4J>
2023-10-13 02:09:04 +0530

    Implemented Basic Auth

--- blog-api-apis/src/main/java/com/codewithgurtej/blog/entities/User.java
+++ blog-api-apis/src/main/java/com/codewithgurtej/blog/entities/User.java
@@ -1,22 +1,23 @@
 package com.codewithgurtej.blog.entities;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name="users")
-public class User {
+public class User implements UserDetails{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
@@ -30,6 +31,45 @@ public class User {
 	
 	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	private List<Post> posts = new ArrayList<>();
+	
+	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinTable(name="user_role",
+		joinColumns=@JoinColumn(name="user", referencedColumnName = "id"),
+		inverseJoinColumns = @JoinColumn(name="role", referencedColumnName = "id")
+	)
+	private Set<Role> roles = new HashSet<>();
+	
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<SimpleGrantedAuthority> authories = this.roles.stream().map((role) -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+		return authories;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 
 	public User() {
 		super();
@@ -84,7 +124,15 @@ public class User {
 		this.posts = posts;
 	}
 
-	public User(int id, String name, String email, String password, String about, List<Post> posts) {
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+	public User(int id, String name, String email, String password, String about, List<Post> posts, Set<Role> roles) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -92,13 +140,12 @@ public class User {
 		this.password = password;
 		this.about = about;
 		this.posts = posts;
+		this.roles = roles;
 	}
 
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", about=" + about
-				+ ", posts=" + posts + "]";
-	}
-	
-	
+				+ ", posts=" + posts + ", roles=" + roles + "]";
+	}	
 }
--- blog-api-apis/src/main/java/com/codewithgurtej/blog/repositories/UserRepo.java
+++ blog-api-apis/src/main/java/com/codewithgurtej/blog/repositories/UserRepo.java
@@ -1,10 +1,12 @@
 package com.codewithgurtej.blog.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.codewithgurtej.blog.entities.User;
 
 public interface UserRepo extends JpaRepository<User, Integer>{
 	
-	
+	Optional<User> findByEmail(String email);
 }
